7c87e41d28486397a3cc3e662489abad5ee46069
Jason Lo <lcmjlo@gmail.com>
2024-11-18 19:07:32 -0600

    Prevent text too long error in embed_work

--- openalex_search/embedding.py
+++ openalex_search/embedding.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 import tiktoken
-from openalex_search.common import CONFIG
+from openalex_search.common import CONFIG, LOGGER
 from openalex_search.db import Work
 
 client = OpenAI()
@@ -17,18 +17,22 @@ def embed_work(work: Work) -> Work:
     work.embedding = embed(str(work))[0]
     return work
 
-def trim_text(text: str, model:str = CONFIG.EMBEDDING_MODEL, tokens: int = 1024) -> str:
+
+def trim_text(text: str, model:str = CONFIG.EMBEDDING_MODEL, max_tokens: int = 1024) -> str:
     """Trim text to a maximum number of tokens."""
-    encoding = tiktoken.encoding_for_model(model)
-    return text[:tokens]
+    encoder = tiktoken.encoding_for_model(model)
+    tokens = encoder.encode(text)
+    trimmed_tokens = tokens[:max_tokens]
+    return encoder.decode(trimmed_tokens)
 
 
 def embed_works(works: list[Work], batch_size: int = 1024) -> list[Work]:
     """Embed a list of works in batch."""
 
     for i in range(0, len(works), batch_size):
+        LOGGER.info(f"Embedding works {i} to {i + batch_size}")
         batch = works[i : i + batch_size]
-        embeddings = embed([str(work) for work in batch])
+        embeddings = embed([trim_text(str(work)) for work in batch])
         for work, embedding in zip(batch, embeddings):
             work.embedding = embedding
     return works
