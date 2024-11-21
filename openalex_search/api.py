03b1f5562b85311ca61cce7d3e1ad91a32b30597
Jason Lo <lcmjlo@gmail.com>
2024-11-21 03:33:53 +0000

    hide no data output

--- openalex_search/api.py
+++ openalex_search/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
 
 from openalex_search import search
 
@@ -20,7 +21,13 @@ def read_root():
     return {"Instruction": "Try /search?query=your_query_here&top_k=3"}
 
 
-@app.get("/search")
+class SearchResults(BaseModel):
+    name: str = Field(validation_alias="display_name")
+    open_alex_url: str = Field(validation_alias="id")
+    orcid: str | None
+
+
+@app.get("/search", response_model=list[SearchResults])
 def search_route(
     query: str = Query(..., title="The query string to search for."),
     top_k: int = Query(3, title="The number of results to return."),
