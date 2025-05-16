5ba1c227966d6634526d243c0b9a15b6b1eb9ee2
Jason Lo <lcmjlo@gmail.com>
2025-05-16 22:04:52 +0000

    clone openalex (working checkpoint)

---
+++ scripts/clone_openalex.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Based on https://docs.openalex.org/download-all-data/upload-to-your-database/load-to-a-relational-database
+source .env
+
+# Variables
+SQL_URL="https://raw.githubusercontent.com/ourresearch/openalex-documentation-scripts/main/openalex-pg-schema.sql"
+SQL_FILE="tmp/openalex-pg-schema.sql"
+FLATTEN_PY_URL="https://raw.githubusercontent.com/ourresearch/openalex-documentation-scripts/refs/heads/main/flatten-openalex-jsonl.py"
+FLATTEN_PY_FILE="tmp/flatten-openalex-jsonl.py"
+
+# Download SQL file
+wget -O $SQL_FILE $SQL_URL
+wget -O $FLATTEN_PY_FILE $FLATTEN_PY_URL
+
+# Step 1: Create the schema
+psql $POSTGRES_URL -f $SQL_FILE
+
+# Download compressed data form S3 bucket
+mc alias set openalex https://s3.amazonaws.com "" ""
+mc mirror --overwrite --remove openalex/openalex openalex-snapshot
+
+# Step 2: Convert the JSON Lines files to CSV
