fd1a3bc2992840cd48da540a2ad9a031cc4bb2a8
Jason Lo <lcmjlo@gmail.com>
2025-05-17 00:01:17 +0000

    add load csv to db step

--- scripts/clone_openalex.sh
+++ scripts/clone_openalex.sh
@@ -7,12 +7,18 @@ source .env
 # Variables
 SQL_URL="https://raw.githubusercontent.com/ourresearch/openalex-documentation-scripts/main/openalex-pg-schema.sql"
 SQL_FILE="tmp/openalex-pg-schema.sql"
+
 FLATTEN_PY_URL="https://raw.githubusercontent.com/ourresearch/openalex-documentation-scripts/refs/heads/main/flatten-openalex-jsonl.py"
 FLATTEN_PY_FILE="tmp/flatten-openalex-jsonl.py"
+
+COPY_SQL_URL="https://raw.githubusercontent.com/ourresearch/openalex-documentation-scripts/refs/heads/main/copy-openalex-csv.sql"
+COPY_SQL_FILE="tmp/copy-openalex-csv.sql"
+
 
 # Download SQL file
 wget -O $SQL_FILE $SQL_URL
 wget -O $FLATTEN_PY_FILE $FLATTEN_PY_URL
+wget -O $COPY_SQL_FILE $COPY_SQL_URL
 
 # Step 1: Create the schema
 psql $POSTGRES_URL -f $SQL_FILE
@@ -23,3 +29,6 @@ mc mirror --overwrite --remove openalex/openalex openalex-snapshot
 
 # Step 2: Convert the JSON Lines files to CSV
 python $FLATTEN_PY_FILE
+
+# Step 3: Load the CSV files into the database
+psql $POSTGRES_URL < $COPY_SQL_FILE
